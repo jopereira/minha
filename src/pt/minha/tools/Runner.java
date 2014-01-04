bcc76dd456e81aa6f7c087a8599f95ae63a5f013
Jose Orlando Pereira <jop@di.uminho.pt>
2014-01-04 14:42:09 +0000

    New date in copyright notice.

--- src/pt/minha/tools/Runner.java
+++ src/pt/minha/tools/Runner.java
@@ -42,7 +42,7 @@ public class Runner {
 			long simulationTime = Long.parseLong(System.getProperty("simulationTime", "0"));
 
 			System.err.println("==== Minha -- middleware testing platform <http://www.minha.pt/>");
-			System.err.println("==== Copyright (c) 2011-2013, Universidade do Minho.");
+			System.err.println("==== Copyright (c) 2011-2014, Universidade do Minho.");
 			System.err.println("==== License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>");
 			System.err.println("==== This is free software: you are free to change and redistribute it.");
 			System.err.println("==== There is NO WARRANTY, to the extent permitted by law.");
