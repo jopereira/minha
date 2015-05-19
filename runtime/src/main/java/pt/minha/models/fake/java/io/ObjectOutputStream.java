99d4768de0ce5f8e80960b10d802a98767d7f14c
Pedro Guimarães <pg22834@alunos.uminho.pt>
2015-05-19 16:17:58 +0100

    Alter implementation of ObjectOutputStream model.
    The ObjectOutputStream model no longer extends DataOutputStream, instead
    it owns a private field of that type that is used to delegate operations.
    This internal change is due to the fact that DataOutputStream methods are
    final, unlike those of ObjectOutputStream; this prevented Minha from
    loading subclasses extending ObjectOutputStream.

--- runtime/src/main/java/pt/minha/models/fake/java/io/ObjectOutputStream.java
+++ runtime/src/main/java/pt/minha/models/fake/java/io/ObjectOutputStream.java
@@ -27,14 +27,14 @@ import java.io.OutputStream;
 import pt.minha.models.local.io.ObjectOutputStreamImpl;
 import pt.minha.models.local.io.OutputStreamInterface;
 
-public class ObjectOutputStream extends DataOutputStream implements ObjectOutput {
-	private OutputStream out;
+public class ObjectOutputStream extends OutputStream implements ObjectOutput {
+	private DataOutputStream out;
 	private ObjectOutputStreamImpl osi;
 
 	public ObjectOutputStream(OutputStream os) throws IOException {
-		super(os);
+		super();
 		
-		this.out = os;
+		this.out = new DataOutputStream(os);
 		this.osi = new ObjectOutputStreamImpl(new OutputStreamInterface() {
 			public void intfWrite(int b) throws IOException {
 				out.write(b);
@@ -58,5 +58,67 @@ public class ObjectOutputStream extends DataOutputStream implements ObjectOutput
 	
 	public void reset() throws IOException {
 		osi.implReset();
+	}
+
+	@Override
+	public void writeBoolean(boolean v) throws IOException {
+		out.writeBoolean(v);
+	}
+
+	@Override
+	public void writeByte(int v) throws IOException {
+		out.writeByte(v);
+	}
+
+	@Override
+	public void writeShort(int v) throws IOException {
+		out.writeShort(v);
+	}
+
+	@Override
+	public void writeChar(int v) throws IOException {
+		out.writeChar(v);
+	}
+
+	@Override
+	public void writeInt(int v) throws IOException {
+		out.writeLong(v);
+	}
+
+	@Override
+	public void writeLong(long v) throws IOException {
+		out.writeLong(v);
+	}
+
+	@Override
+	public void writeFloat(float v) throws IOException {
+		out.writeFloat(v);
+	}
+
+	@Override
+	public void writeDouble(double v) throws IOException {
+		out.writeDouble(v);
+	}
+
+	@Override
+	public void writeBytes(String s) throws IOException {
+		out.writeBytes(s);
+	}
+
+	@Override
+	public void writeChars(String s) throws IOException {
+		out.writeChars(s);
+	}
+
+	@Override
+	public void writeUTF(String s) throws IOException {
+		out.writeUTF(s);
+	}
+
+	@Override
+	public void close() throws IOException {
+        flush();
+        osi.close();
+        out.close();
 	}
 }
